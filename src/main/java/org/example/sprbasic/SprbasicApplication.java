271248e8755d64ebe293fbc2a00f6894a26aab27
hayoung <kanghayoung527@gmail.com>
2025-07-02 10:26:05 +0900

    Initial commit

---
+++ src/main/java/org/example/sprbasic/SprbasicApplication.java
@@ -0,0 +1,13 @@
+package org.example.sprbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprbasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprbasicApplication.class, args);
+    }
+
+}
