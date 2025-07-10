80fc40fcd8544c918a377b42d66d0733801f479e
hayoung <kanghayoung527@gmail.com>
2025-07-10 09:55:39 +0900

    change interface

--- src/main/java/org/example/sprbasic/repository/BoardRepository.java
+++ src/main/java/org/example/sprbasic/repository/BoardRepository.java
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 // Jpa는 Board라는 Entity와 관련있다.(위처럼 상속만해도 사용 가능)   <generic>에는 class 자료형만 가능.
-
 }
